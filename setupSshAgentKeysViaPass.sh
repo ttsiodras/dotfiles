abea6c8f283fb60a6f134cc8dc0dbb126fe5b0cb
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-11-23 18:31:14 +0200

    Adaptively use either pass or ssh-add

--- setupSshAgentKeysViaPass.sh
+++ setupSshAgentKeysViaPass.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-COOL=$(pass Home/SSH-Agent)
+if [ -d $HOME/.password-store ] ; then
+    COOL=$(pass Home/SSH-Agent)
 /usr/bin/expect <<OEF
 spawn ssh-add
 expect "Enter passphrase for"
 send "$COOL\n"
 interact
 OEF
+else
+    ssh-add
+fi
