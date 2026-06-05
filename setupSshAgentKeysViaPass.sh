1486618692091620ad025bf165132757384c7ca1
Thanassis Tsiodras <thanassis@mathrix.com>
2026-06-05 15:49:51 +0200

    Fallback to interactive prompt if no pass store available

--- setupSshAgentKeysViaPass.sh
+++ setupSshAgentKeysViaPass.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-if [ -d $HOME/.password-store ] ; then
-    COOL=$(pass Home/SSH-Agent)
-/usr/bin/expect <<OEF
+set -u
+COOL=""
+[ -d "$HOME/.password-store" ] && COOL=$(pass Home/SSH-Agent)
+
+if [ -n "$COOL" ] ; then
+    # Feed the passphrase to ssh-add via expect.
+    /usr/bin/expect <<OEF
 spawn ssh-add
 expect "Enter passphrase for"
 send "$COOL\n"
 interact
 OEF
 else
+    # No password store, or empty entry: fall back to an interactive prompt.
     ssh-add
 fi
